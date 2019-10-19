8200dccf39f06667f592c7e8691fc1499f22c4c8
osboxes.org <mateuscoelho2009@gmail.com>
2019-10-18 21:19:56 -0400

    fix: fixes dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 # Extend from the official Elixir image
 FROM elixir:latest
 
+RUN apt-get update && \
+  apt-get install -y postgresql-client
+
 # Create app directory and copy the Elixir projects into it
 RUN mkdir /app
 COPY . /app
@@ -12,3 +15,6 @@ RUN mix local.hex --force
 
 # Compile the project
 RUN mix do compile
+RUN chmod 755 entrypoint.sh
+
+CMD ["/app/entrypoint.sh"]
---
+++ lib/app/repo.ex
@@ -0,0 +1,15 @@
+defmodule App.Repo do
+  use Ecto.Repo,
+    otp_app: :app,
+    adapter: Ecto.Adapters.Postgres
+
+  def init(_, config) do
+    config = config
+      |> Keyword.put(:username, System.get_env("PGUSER"))
+      |> Keyword.put(:password, System.get_env("PGPASSWORD"))
+      |> Keyword.put(:database, System.get_env("PGDATABASE"))
+      |> Keyword.put(:hostname, System.get_env("PGHOST"))
+      |> Keyword.put(:port, System.get_env("PGPORT") |> String.to_integer)
+    {:ok, config}
+  end
+end
